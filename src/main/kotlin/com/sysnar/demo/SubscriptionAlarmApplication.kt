33424f97391b81132dafcea317fd03be1c974d43
sysnar <sysn4r@gmail.com>
2023-03-31 23:42:55 +0900

    init project

---
+++ src/main/kotlin/com/sysnar/demo/SubscriptionAlarmApplication.kt
@@ -0,0 +1,11 @@
+package com.sysnar.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubscriptionAlarmApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubscriptionAlarmApplication>(*args)
+}
---
+++ src/test/kotlin/com/sysnar/demo/SubscriptionAlarmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sysnar.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubscriptionAlarmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
